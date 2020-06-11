6423354a9180afd435aea6b8baf6cd1e156b666b
new <new@DESKTOP-I6ETB8S>
2020-06-11 11:27:51 +0530

    This is exercise 3

---
+++ Milestone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,12 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+	public static void main(String args[]) {
+		int p =Integer.parseInt(args[0]);
+		int q =Integer.parseInt(args[1]);
+		int r=p+q;
+		System.out.println("The Sum of " +p+ " and " +q+ " is " +r);
+		
+	}
+
+}
