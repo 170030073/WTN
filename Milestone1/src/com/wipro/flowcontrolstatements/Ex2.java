a92517b5361f92bda74e6e5c83521956dd58eb76
new <new@DESKTOP-I6ETB8S>
2020-06-12 07:43:34 +0530

    Exercise 2 in flow control statements

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,12 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 {
+	public static void main(String args[]) {
+		int p=Integer.parseInt(args[0]);
+		if(p%2==0)
+			System.out.println("The given number is even");
+		if(p%2!=0)
+			System.out.println("The given number is odd");
+	}
+
+}
