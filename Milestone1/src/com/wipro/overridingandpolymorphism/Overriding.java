89b51b27315e46b2946a55f40f30e082f49b8ac5
new <new@DESKTOP-I6ETB8S>
2020-06-24 19:46:04 +0530

    Exercise 2 in overriding

---
+++ Milestone1/src/com/wipro/overridingandpolymorphism/Overriding.java
@@ -0,0 +1,49 @@
+package com.wipro.overridingandpolymorphism;
+
+class Shape{
+	void draw() {
+		System.out.println("drawing shape");
+	}
+	void erase() {
+		System.out.println("ersing shape");
+	}
+}
+class Circle extends Shape{
+	void draw() {
+		System.out.println("drawing circle");
+	}
+	void erase() {
+		System.out.println("erasing circle");
+	}
+}
+class Traingle extends Shape{
+	void draw() {
+		System.out.println("drawing triangle");
+	}
+	void erase() {
+		System.out.println("erasing triangle");
+	}
+}
+class Square extends Shape{
+	void draw() {
+		System.out.println("drawing square");
+	}
+	void erase() {
+		System.out.println("erasing square");
+	}
+}
+
+public class Overriding {
+	public static void main(String args[]) {
+		Shape c=new Circle();
+		Shape t=new Traingle();
+		Shape s=new Square();
+		c.draw();
+		c.erase();
+		t.draw();
+		t.erase();
+		s.draw();
+		s.erase();
+	}
+
+}
