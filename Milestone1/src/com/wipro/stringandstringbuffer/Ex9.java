8c47b7d3cadefa08ae4da36367fa7b3da3aa30d8
new <new@DESKTOP-I6ETB8S>
2020-06-25 09:34:04 +0530

    Exercise 6 in string and string buffer in pbl app

---
+++ Milestone1/src/com/wipro/stringandstringbuffer/Ex9.java
@@ -0,0 +1,13 @@
+package com.wipro.stringandstringbuffer;
+
+public class Ex9 {
+	public static void main (String[] args) 
+    {
+        String a = args[0];
+        String b = args[1];
+        if(a.length() > b.length())
+        System.out.println(b+a+b);
+        else
+        System.out.println(a+b+a);
+    }
+}
