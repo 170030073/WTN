05b18a6bb4ff97980e37f331e02b484dad50c4d5
new <new@DESKTOP-I6ETB8S>
2020-07-03 17:36:53 +0530

    Exercise 1 in iostreams

---
+++ Milestone1/src/com/wipro/iostreams/Ex1.java
@@ -0,0 +1,34 @@
+package com.wipro.iostreams;
+import java.io.*;
+import java.util.*;
+public class Ex1 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter file name");
+		String filename = sc.nextLine();
+		System.out.println("Enter the character to be counted");
+		char needle = sc.nextLine().charAt(0);
+	
+		File file = new File(filename);
+		int charCount = 0;
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		int ch;
+		do {
+			ch = br.read();
+			
+			if (ch >= 65 && ch <= 90) ch += 32;
+			if (needle >= 65 && needle <= 90) needle += 32;
+			
+			if (ch == needle)
+				charCount++;
+		} while (ch != -1);
+				
+		System.out.println("File has " +
+				charCount + " instances of letter '" + needle + "'.");
+		br.close();
+		sc.close();
+	}
+
+}
